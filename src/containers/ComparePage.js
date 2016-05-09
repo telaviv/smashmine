a433badd9dc85ba4455387d6ba4ea583a27bf7e1
Shawn Krisman <krisman.shawn@gmail.com>
2016-05-08 18:45:00 -0700

    we can submit the event.

--- src/containers/ComparePage.js
+++ src/containers/ComparePage.js
@@ -1,16 +1,26 @@
+import { connect } from 'react-redux';
 import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
+import * as CompareActions from '../actions/compare';
 
 
 class CompareForm extends Component {
   static propTypes = {
     fields: PropTypes.object.isRequired,
+    dispatch: PropTypes.func.isRequired,
+  }
+
+  submit(e) {
+    e.preventDefault();
+    const { fields: { player1, player2 }, dispatch } = this.props;
+    const action = CompareActions.submitCompare(player1, player2);
+    dispatch(action);
   }
 
   render() {
     const { fields: { player1, player2 } } = this.props;
     return (
-      <form name="compare">
+      <form name="compare" onSubmit={this.submit.bind(this)} >
         <input className="player1" type="text" placeholder="Shaky" {...player1} />
         <input className="player2" type="text" placeholder="Trevonte" {...player2} />
         <button type="submit">Submit</button>
@@ -19,7 +29,10 @@ class CompareForm extends Component {
   }
 }
 
-export default reduxForm({
+CompareForm = reduxForm({
   form: 'compare',
   fields: ['player-1', 'player-2'],
 })(CompareForm);
+
+
+export default connect()(CompareForm);
