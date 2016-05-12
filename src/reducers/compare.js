d255204974fa9f23d9ea181e64bc0caaae234abd
Shawn Krisman <krisman.shawn@gmail.com>
2016-05-12 00:18:29 -0700

    finished RECIEVE_COMPARISON code.

--- src/reducers/compare.js
+++ src/reducers/compare.js
@@ -1,9 +1,16 @@
-import { REQUEST_COMPARISON } from '../actions/compare';
+import { REQUEST_COMPARISON, RECIEVE_COMPARISON } from '../actions/compare';
 
 export default function compare(state = null, action) {
   switch (action.type) {
     case REQUEST_COMPARISON:
-      return { isFetching: true }
+      return { isFetching: true };
+    case RECIEVE_COMPARISON:
+      return {
+        isFetching: false,
+        player1: action.data.players[0],
+        player2: action.data.players[1],
+        matches: action.data.matches,
+      };
     default:
       return state;
   }
