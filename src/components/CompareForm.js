47fabf62d413c0c64adf7ecc0d47da8918874dda
Shawn Krisman <krisman.shawn@gmail.com>
2016-05-22 09:36:55 -0700

    moved CompareForm to it's own file.

---
+++ src/components/CompareForm.js
@@ -0,0 +1,34 @@
+import React, { Component, PropTypes } from 'react';
+import { reduxForm } from 'redux-form';
+import * as CompareActions from '../actions/compare';
+
+
+class CompareForm extends Component {
+  static propTypes = {
+    fields: PropTypes.object.isRequired,
+    dispatch: PropTypes.func.isRequired,
+  }
+
+  submit(e) {
+    e.preventDefault();
+    const { fields: { player1, player2 }, dispatch } = this.props;
+    const action = CompareActions.submitCompare(player1.value, player2.value);
+    dispatch(action);
+  }
+
+  render() {
+    const { fields: { player1, player2 } } = this.props;
+    return (
+      <form name="compare" onSubmit={this.submit.bind(this)} >
+        <input className="player1" type="text" placeholder="Shaky" {...player1} />
+        <input className="player2" type="text" placeholder="Trevonte" {...player2} />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'compare',
+  fields: ['player1', 'player2'],
+})(CompareForm);
--- src/containers/ComparePage.js
+++ src/containers/ComparePage.js
@@ -1,39 +1,7 @@
 import { connect } from 'react-redux';
 import React, { Component, PropTypes } from 'react';
-import { reduxForm } from 'redux-form';
 import { If, Then, Else } from 'react-if';
-import * as CompareActions from '../actions/compare';
-
-
-class CompareForm extends Component {
-  static propTypes = {
-    fields: PropTypes.object.isRequired,
-    dispatch: PropTypes.func.isRequired,
-  }
-
-  submit(e) {
-    e.preventDefault();
-    const { fields: { player1, player2 }, dispatch } = this.props;
-    const action = CompareActions.submitCompare(player1.value, player2.value);
-    dispatch(action);
-  }
-
-  render() {
-    const { fields: { player1, player2 } } = this.props;
-    return (
-      <form name="compare" onSubmit={this.submit.bind(this)} >
-        <input className="player1" type="text" placeholder="Shaky" {...player1} />
-        <input className="player2" type="text" placeholder="Trevonte" {...player2} />
-        <button type="submit">Submit</button>
-      </form>
-    );
-  }
-}
-
-CompareForm = reduxForm({
-  form: 'compare',
-  fields: ['player1', 'player2'],
-})(CompareForm);
+import CompareForm from '../components/CompareForm';
 
 
 function mapStateToProps(state) {
