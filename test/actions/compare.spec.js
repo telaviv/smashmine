3963d45d7f42bc24c1b72b9573a96063d467e872
Shawn Krisman <krisman.shawn@gmail.com>
2016-05-23 05:00:20 -0700

    test for api call.

--- test/actions/compare.spec.js
+++ test/actions/compare.spec.js
@@ -7,10 +7,22 @@ describe('submitCompare', () => {
     const fetch = stub().returns(new Promise(spy()));
     const dispatch = spy();
     const p1 = 'p1';
-    const p2 = 'p2'
+    const p2 = 'p2';
 
     submitCompare(p1, p2, fetch)(dispatch);
 
     expect(dispatch).to.have.been.calledWith({ type: REQUEST_COMPARISON });
+  });
+
+  it('fetches from the api', () => {
+    const fetch = stub().returns(new Promise(spy()));
+    const dispatch = spy();
+    const p1 = 'p1';
+    const p2 = 'p2';
+
+    submitCompare(p1, p2, fetch)(dispatch);
+
+    expect(fetch).to.have.been.calledWith(
+      'http://localhost:3001/compare?player1=p1&player2=p2');
   });
 });
