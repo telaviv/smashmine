89d5f126a72f88fee854bcbf3afc42e0db58caa4
Shawn Krisman <krisman.shawn@gmail.com>
2016-05-23 05:55:30 -0700

    changed name of callable to fetch.

--- test/actions/compare.spec.js
+++ test/actions/compare.spec.js
@@ -6,34 +6,35 @@ class MockFetch {
   constructor(payload) {
     this._payload = payload;
     this._resolver = spy();
+    this._fetch = stub().returns(new Promise(this._resolver));
   }
 
-  callable() {
-    return stub().returns(new Promise(this._resolver));
+  get fetch() {
+    return this._fetch;
   }
 }
 
 describe('submitCompare', () => {
   it('immediately dispatches a request comparison', () => {
-    const fetch = (new MockFetch()).callable();
+    const fetch = new MockFetch()
     const dispatch = spy();
     const p1 = 'p1';
     const p2 = 'p2';
 
-    submitCompare(p1, p2, fetch)(dispatch);
+    submitCompare(p1, p2, fetch.fetch)(dispatch);
 
     expect(dispatch).to.have.been.calledWith({ type: REQUEST_COMPARISON });
   });
 
   it('fetches from the api', () => {
-    const fetch = (new MockFetch()).callable();
+    const fetch = new MockFetch()
     const dispatch = spy();
     const p1 = 'p1';
     const p2 = 'p2';
 
-    submitCompare(p1, p2, fetch)(dispatch);
+    submitCompare(p1, p2, fetch.fetch)(dispatch);
 
-    expect(fetch).to.have.been.calledWith(
+    expect(fetch.fetch).to.have.been.calledWith(
       'http://localhost:3001/compare?player1=p1&player2=p2');
   });
 });
