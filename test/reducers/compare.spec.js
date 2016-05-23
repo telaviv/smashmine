860fe11d817aa2f102c2eb51b45d3da3509dd4ca
Shawn Krisman <krisman.shawn@gmail.com>
2016-05-22 18:53:55 -0700

    fixed some test linting.

--- test/reducers/compare.spec.js
+++ test/reducers/compare.spec.js
@@ -1,5 +1,4 @@
-import chai from 'chai'
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import shallowDeepEqual from 'chai-shallow-deep-equal';
 import compare from '../../src/reducers/compare';
 import { REQUEST_COMPARISON, RECIEVE_COMPARISON } from '../../src/actions/compare';
@@ -9,23 +8,23 @@ chai.use(shallowDeepEqual);
 describe('reducers', () => {
   describe('counter', () => {
     it('handles initial state', () => {
-      expect(compare(undefined, {})).to.deep.equal({cachedComparisons: {}});
+      expect(compare(undefined, {})).to.deep.equal({ cachedComparisons: {} });
     });
 
     it('handles REQUEST_COMPARISON', () => {
-      expect(compare(null, { type: REQUEST_COMPARISON}))
-        .to.deep.equal({isFetching: true, fetchedComparison: null});
+      expect(compare(null, { type: REQUEST_COMPARISON }))
+        .to.deep.equal({ isFetching: true, fetchedComparison: null });
     });
 
     describe('RECIEVE_COMPARISON', () => {
-      const originalComparison = {'sean-trevonte': null};
+      const originalComparison = { 'sean-trevonte': null };
       const newComparison = {
         'sean-shaky': {
           player1: 'sean',
           player2: 'shaky',
           matches: [],
           winPercentage: 0.04,
-        }
+        },
       };
       const data = {
         player1: 'sean',
@@ -37,30 +36,29 @@ describe('reducers', () => {
       it('merges the comparison into the cache', () => {
         const mergedComparisons = Object.assign({}, originalComparison, newComparison);
         const state = compare(
-          {cachedComparisons: originalComparison},
-          {type: RECIEVE_COMPARISON, data},
+          { cachedComparisons: originalComparison },
+          { type: RECIEVE_COMPARISON, data },
         );
         expect(state.cachedComparisons).to.shallowDeepEqual(mergedComparisons);
-      })
+      });
 
       it('has a copy of fetchedComparison in cachedComparisons', () => {
         const state = compare(
-          {cachedComparisons: originalComparison},
-          {type: RECIEVE_COMPARISON, data},
+          { cachedComparisons: originalComparison },
+          { type: RECIEVE_COMPARISON, data },
         );
         expect(state.fetchedComparison).to.deep.equal(
           state.cachedComparisons['sean-shaky'],
-        )
-      })
+        );
+      });
 
       it('includes a timestamp in the comparisons', () => {
         const state = compare(
-          {cachedComparisons: originalComparison},
-          {type: RECIEVE_COMPARISON, data},
+          { cachedComparisons: originalComparison },
+          { type: RECIEVE_COMPARISON, data },
         );
         expect(state.fetchedComparison).to.include.keys('updatedAt');
-      })
+      });
     });
-
   });
 });
