2f5009b67f9d707737a9e5b00325afd701006f28
Shawn Krisman <krisman.shawn@gmail.com>
2016-05-24 14:58:02 -0700

    switched our mockfetch implementation.

--- test/actions/compare.spec.js
+++ test/actions/compare.spec.js
@@ -2,63 +2,45 @@ import { expect } from 'chai';
 import { stub, spy, useFakeTimers } from 'sinon';
 import { submitCompare, REQUEST_COMPARISON, RECIEVE_COMPARISON } from '../../src/actions/compare';
 
-class MockFetch {
-  constructor(payload) {
-    this._resolver = spy();
-    this._promise = new Promise(this._resolver);
-    this._fetch = stub().returns(this._promise);
-  }
-
-  resolve(payload) {
-    this._resolve({json: () => payload});
-  }
-
-  get fetch() {
-    return this._fetch;
-  }
-
-  get _resolve() {
-    return this._resolver.args[0][0];
-  }
+function mockFetch(data) {
+  return stub().returns(Promise.resolve({json: () => data}));
 }
 
 describe('submitCompare', () => {
   it('immediately dispatches a request comparison', () => {
-    const fetch = new MockFetch();
+    const fetch = mockFetch();
     const dispatch = spy();
     const p1 = 'p1';
     const p2 = 'p2';
 
-    submitCompare(p1, p2, fetch.fetch)(dispatch);
+    submitCompare(p1, p2, fetch)(dispatch);
 
     expect(dispatch).to.have.been.calledWith({ type: REQUEST_COMPARISON });
   });
 
 
   it('fetches from the api', () => {
-    const fetch = new MockFetch();
+    const fetch = mockFetch();
     const dispatch = spy();
     const p1 = 'p1';
     const p2 = 'p2';
 
-    submitCompare(p1, p2, fetch.fetch)(dispatch);
+    submitCompare(p1, p2, fetch)(dispatch);
 
-    expect(fetch.fetch).to.have.been.calledWith(
+    expect(fetch).to.have.been.calledWith(
       'http://localhost:3001/compare?player1=p1&player2=p2');
   });
 
-  it('dispatches RECIEVE_COMPARISON on success', (done) => {
+  it('dispatches RECIEVE_COMPARISON on success', () => {
     const dispatch = spy();
     const p1 = 'p1';
     const p2 = 'p2';
     const data = {player1: {name: p1}, player2: {name: p2}};
-
-    fetch = stub().returns(Promise.resolve({json: () => data}));
+    const fetch = mockFetch(data);
 
     return submitCompare(p1, p2, fetch)(dispatch).then(() => {
       expect(dispatch).to.have.been.calledWith(
         { type: RECIEVE_COMPARISON, data });
-      done();
-    }).catch(done);
+    });
   });
 });
