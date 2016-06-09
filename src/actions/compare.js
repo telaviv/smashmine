92c6ce1d51d9e9472e377ddb4e5e910c2e39c654
Shawn Krisman <krisman.shawn@gmail.com>
2016-06-09 01:26:07 -0700

    moved compare code over to apifetch.

--- src/actions/compare.js
+++ src/actions/compare.js
@@ -1,7 +1,6 @@
 /* eslint no-console: 0 */
-import ifetch from 'isomorphic-fetch';
+import apiFetch from '../utils/api';
 import { push } from 'react-router-redux';
-import { SubmissionError } from 'redux-form';
 import URI from 'urijs';
 import 'urijs/src/URITemplate';
 
@@ -25,61 +24,11 @@ function compareRedirectURL(player1, player2) {
   return uri.toString();
 }
 
-function compareAPIURL(player1, player2) {
-  return new URI('http://localhost:3001/compare')
-    .query({ player1, player2 })
-    .toString();
-}
-
-function normalizeServerErrors(error) {
-  const errors = error.errors;
-  const normalized = {};
-  for (const [key, value] of Object.entries(errors)) {
-    normalized[key] = value.msg;
-  }
-  return normalized;
-}
-
-function handleServerErrors(response) {
-  if (response.ok) {
-    return response;
-  }
-
-  return response
-    .json()
-    .then(err => {
-      const errors = normalizeServerErrors(err);
-      throw new SubmissionError(errors);
-    })
-    .catch((err) => {
-      if (!(err instanceof SubmissionError)) {
-        throw new SubmissionError(
-          { _error: "Something wen't wrong please try again later" }
-        );
-      }
-      throw err;
-    });
-}
-
-function handleFetchErrors(err) {
-  console.error(err);
-  if (err instanceof TypeError) {
-    throw new SubmissionError(
-      { _error: "Something wen't wrong please try again later" }
-    );
-  } else {
-    throw err;
-  }
-}
-
-export function submitCompare(player1, player2, fetch = ifetch) {
+export function submitCompare(player1, player2, fetch = apiFetch) {
   return (dispatch) => {
     dispatch(requestComparison());
 
-    return fetch(compareAPIURL(player1, player2))
-      .catch(handleFetchErrors)
-      .then(handleServerErrors)
-      .then(response => response.json())
+    return fetch('compare', { player1, player2 })
       .then(json => {
         dispatch(receiveComparison(json));
         dispatch(push(compareRedirectURL(json.player1.name, json.player2.name)));
