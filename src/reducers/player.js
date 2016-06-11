f4d83745627f9baac13c6fb96142e4e3cf49d956
Shawn Krisman <krisman.shawn@gmail.com>
2016-06-11 10:16:38 -0700

    we start fetching on REQUEST_PLAYER_INFO

--- src/reducers/player.js
+++ src/reducers/player.js
@@ -1,3 +1,10 @@
-export default function player(state = { isFetching: false }) {
-  return state;
+import { REQUEST_PLAYER_INFO } from '../../src/actions/player';
+
+export default function player(state = { isFetching: false }, action) {
+  switch (action.type) {
+    case REQUEST_PLAYER_INFO:
+      return { isFetching: true };
+    default:
+      return state;
+  }
 }
--- test/reducers/player.spec.js
+++ test/reducers/player.spec.js
@@ -1,10 +1,16 @@
 import { expect } from 'chai';
 import player from '../../src/reducers/player';
+import { REQUEST_PLAYER_INFO } from '../../src/actions/player';
 
 describe('reducers', () => {
   describe('player', () => {
     it('handles initial state', () => {
       expect(player(undefined, {})).to.deep.equal({ isFetching: false });
+    });
+    it('handles REQUEST_PLAYER_INFO', () => {
+      expect(player(undefined, { type: REQUEST_PLAYER_INFO })).to.deep.equal(
+        { isFetching: true },
+      );
     });
   });
 });
