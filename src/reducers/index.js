1546fab5462f3d4ed6493bdbc2887764ddcca823
Shawn Krisman <krisman.shawn@gmail.com>
2016-06-11 23:10:26 -0700

    install player reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,11 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import { routerReducer as routing } from 'react-router-redux';
 import compare from './compare';
-
+import player from './player';
 
 const rootReducer = combineReducers({
   compare,
+  player,
   routing,
   form,
 });
